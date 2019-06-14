64633a35f0bec7f402b9370da63901e3cccf87af
Luis Souza <luis_csouza@uol.com.br>
2019-06-13 22:28:59 -0300

    MIGRATION

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -22,8 +22,8 @@ namespace QuickBuy.Dominio.Entidades
         public int NumeroEndereco { get; set; }
 
         public int FormapagementoID { get; set; }
-        public FormaPagamento FormaPagamento { get; set; }
-        public ICollection<ItemPedido> ItensPedido { get; set; }
+        public virtual FormaPagamento FormaPagamento { get; set; }
+        public virtual ICollection<ItemPedido> ItensPedido { get; set; }
 
         public override void Validate()
         {
---
+++ QuickBuy.Repositorio/Migrations/20190614011114_PrimeiraVersaoBase.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickBuy.Repositorio.Migrations
+{
+    public partial class PrimeiraVersaoBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormaPagemento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    Descricao = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormaPagemento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    Descricao = table.Column<string>(maxLength: 400, nullable: false),
+                    Preco = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    Senha = table.Column<string>(maxLength: 400, nullable: false),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    Sobrenome = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false),
+                    DataPedido = table.Column<DateTime>(nullable: false),
+                    UsuarioId = table.Column<int>(nullable: false),
+                    DataPrevisaoEntrega = table.Column<DateTime>(nullable: false),
+                    Cep = table.Column<string>(maxLength: 10, nullable: false),
+                    Estado = table.Column<string>(maxLength: 100, nullable: false),
+                    Cidade = table.Column<string>(maxLength: 100, nullable: false),
+                    EnderecoCompleto = table.Column<string>(maxLength: 100, nullable: false),
+                    NumeroEndereco = table.Column<int>(nullable: false),
+                    FormapagementoID = table.Column<int>(nullable: false),
+                    FormaPagamentoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_FormaPagemento_Form~",
+                        column: x => x.FormaPagamentoId,
+                        principalTable: "FormaPagemento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemPedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    ProdutoId = table.Column<int>(nullable: false),
+                    Quantidade = table.Column<decimal>(nullable: false),
+                    PedidoID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemPedidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemPedidos_Pedidos_PedidoID",
+                        column: x => x.PedidoID,
+                        principalTable: "Pedidos",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemPedidos_PedidoID",
+                table: "ItemPedidos",
+                column: "PedidoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_FormaPagamentoId",
+                table: "Pedidos",
+                column: "FormaPagamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_UsuarioId",
+                table: "Pedidos",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemPedidos");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "FormaPagemento");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
