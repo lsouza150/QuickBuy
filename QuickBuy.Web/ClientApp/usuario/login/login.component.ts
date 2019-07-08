149c98675901ad46baf0887c355753562b1290bf
Luis Souza <luis.souza@poliview.com.br>
2019-07-08 12:55:57 -0300

    Criando ComponentesLogin

--- QuickBuy.Web/ClientApp/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/usuario/login/login.component.ts
@@ -1,1 +1,13 @@
-﻿
+import { Component, TemplateRef } from "@angular/Core";
+
+@Component({
+  selector: "api-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+
+})
+
+export class LoginComponent {
+
+
+}
